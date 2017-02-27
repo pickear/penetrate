915f91df53a5fc1b4743722201f0fa7fb5754d89
pickear <pickear@gmail.com>
2017-02-28 00:00:23 +0800

    换页面

--- home/src/main/resources/static/js/nav.js
+++
@@ -1,5 +0,0 @@
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-});
