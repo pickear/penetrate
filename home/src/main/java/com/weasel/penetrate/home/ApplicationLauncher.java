5bf607352da2dcde4621da50b722ee11413c49d2
pickear <pickear@gmail.com>
2017-03-01 17:01:12 +0800

    添加注释

--- home/src/main/java/com/weasel/penetrate/home/ApplicationLauncher.java
+++ home/src/main/java/com/weasel/penetrate/home/ApplicationLauncher.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 /**
  * 启动方式:java -jar penetrate-home.jar -Dspring.profiles.active=prod|dev|test
@@ -17,8 +18,13 @@ public class ApplicationLauncher {
 	private final static String DEFAULT_ENV = "dev";
 
 	public static void main(String[] args) {
-		String runtimeEnv = System.getProperty("runtime.env",DEFAULT_ENV);
-		System.setProperty("runtime.env",runtimeEnv);
+		String runtimeEnv = System.getProperty("spring.profiles.active");
+		if(StringUtils.isEmpty(runtimeEnv)){
+			log.warn("找不到环境参数设置，使用默认环境[{}]，如果需要设置环境参数，在启动脚本加上如:[{}]",DEFAULT_ENV,"-Dspring.profiles.active=test");
+			runtimeEnv = DEFAULT_ENV;
+		}
+
+		System.setProperty("spring.profiles.active",runtimeEnv);
 		log.info("当前运行环境是[{}]",runtimeEnv);
 		log.info("当前操作系统[{}]", SystemHelper.getOSname());
 
